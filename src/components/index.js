cad8c817a6037afce2d3734799ff9b6a1caadecb
daocam <camara.daouda@outlook.fr>
2023-08-08 14:19:08 -0400

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import NavBar from './NavBar';
+import Hero from './Hero';
+import Stats from './Stats';
+import CTA from './CTA';
+import Footer from './Footer';
+import Clients from './Clients';
+import Billing from './Billing';
+import CardDeal from './CardDeal';
+import Business from './Business';
+import Testimonials from './Testimonials';
+
+export {
+  NavBar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer
+};
